6fcb48cdc7499a089bfcafa09972fb23470761b6
DenUsm <denchikiiii191@gmail.com>
2019-02-07 14:51:14 +0700

    Add method GetDifference

--- CourseTask/Range/Program.cs
+++ CourseTask/Range/Program.cs
@@ -6,14 +6,37 @@ namespace Range
     {
         static void Main(string[] args)
         {
-            Range firstInterval = new Range(2, 6);
-            Range secondInterval = new Range(5, 13);
+            Range firstInterval = new Range(100, 300);
+            Range secondInterval = new Range(280, 400);
 
-            Range inte = firstInterval.GetIntersection(secondInterval);
-            Range inte1 = secondInterval.GetIntersection(firstInterval);
+            Range intersection = firstInterval.GetIntersection(secondInterval);
+            if (intersection == null)
+            {
+                Console.WriteLine("Пересечение отсутствует");
+            }
+            else
+            {
+                Console.WriteLine("Пересечения [{0},{1}]", intersection.From, intersection.To);
+            }
 
             Range[] unionInterval = firstInterval.GetUnion(secondInterval);
-            Range[] unionInterval2 = secondInterval.GetUnion(firstInterval);
+            for (int i = 0; i < unionInterval.Length; i++)
+            {
+                Console.WriteLine("{0} объединение [{1},{2}]", i + 1, unionInterval[i].From, unionInterval[i].To);
+            }
+
+            Range[] differenceInterval = secondInterval.GetDifference(firstInterval);
+            if (differenceInterval == null)
+            {
+                Console.WriteLine("Результат отсутствует");
+            }
+            else
+            {
+                for (int i = 0; i < differenceInterval.Length; i++)
+                {
+                    Console.WriteLine("{0} разность [{1},{2}]", i + 1, differenceInterval[i].From, differenceInterval[i].To);
+                }
+            }
 
             Console.ReadKey();
         }
--- CourseTask/Range/Range.cs
+++ CourseTask/Range/Range.cs
@@ -23,7 +23,7 @@
 
         public Range GetIntersection(Range interval)
         {
-            if((interval.From > To) || (interval.To < From))
+            if ((interval.From > To) || (interval.To < From))
             {
                 return null;
             }
@@ -51,6 +51,30 @@
             }
             return new Range[] { new Range(GetMin(currentInterval.From, interval.From), GetMax(currentInterval.To, interval.To)) };
         }
+
+        public Range[] GetDifference(Range interval)
+        {
+            Range currentInterval = new Range(From, To);
+            Range intersectionInterval = interval.GetIntersection(currentInterval);
+
+            if (intersectionInterval == null)
+            {
+                return new Range[] { currentInterval };
+            }
+            else if ((interval.From > currentInterval.From) && (interval.To > currentInterval.To))
+            {
+                return new Range[] { new Range(currentInterval.From, interval.From) };
+            }
+            else if ((interval.From < currentInterval.From) && (interval.To < currentInterval.To))
+            {
+                return new Range[] { new Range(interval.To, currentInterval.To) };
+            }
+            else if ((currentInterval.From < interval.From) && (currentInterval.To > interval.To))
+            {
+                return new Range[] { new Range(currentInterval.From, interval.From), new Range(interval.To, currentInterval.To) };
+            }
+            return null;
+        }
+
     }
 }
- 

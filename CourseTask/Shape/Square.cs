e7a8adbb2ec3343e38acf25721a1dd240dab5d2b
Denis <denchikiiiiUsm@yandex.ru>
2019-02-07 21:34:13 +0700

    Add Square class

---
+++ CourseTask/Shape/Square.cs
@@ -0,0 +1,32 @@
+﻿namespace Shape
+{
+    class Square: IShape
+    {
+        public double Side { get; set; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+        public double GetWidth()
+        {
+            return Side;
+        }
+
+        public double GetHeigth()
+        {
+            return Side;
+        }
+
+        public double GetArea()
+        {
+            return Side * Side;
+        }
+
+        public double GetPerimeter()
+        {
+            return 4 * Side;
+        }
+    }
+}

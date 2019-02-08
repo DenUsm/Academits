9ea53ffb5e1c06498b0265142f2083c4500547a2
DenUsm <denchikiiii191@gmail.com>
2019-02-08 17:57:49 +0700

    Create Comparer class for sorting Area and Perimeter

---
+++ CourseTask/Shape/ShapesComparerArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Shape
+{
+    class ShapesComparerArea : IComparer<Shape>
+    {
+        public int Compare(Shape firstShape, Shape secondShape)
+        {
+            if(firstShape.GetArea() > secondShape.GetArea())
+            {
+                return -1;
+            }
+            else if(firstShape.GetArea() < secondShape.GetArea())
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ CourseTask/Shape/ShapesComparerPerimeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Shape
+{
+    class ShapesComparerPerimeter : IComparer<Shape>
+    {
+        public int Compare(Shape firstShape, Shape secondShape)
+        {
+            if(firstShape.GetPerimeter() > secondShape.GetPerimeter())
+            {
+                return -1;
+            }
+            else if(firstShape.GetPerimeter() < secondShape.GetPerimeter())
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}

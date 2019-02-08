b7e14547fb7679c57cdd056365fc03510b81786a
DenUsm <denchikiiii191@gmail.com>
2019-02-08 14:43:47 +0700

    Create some shapes

--- CourseTask/Shape/Program.cs
+++ CourseTask/Shape/Program.cs
@@ -6,39 +6,17 @@ namespace Shape
     {
         static void Main(string[] args)
         {
-            Square res = new Square(6.25);
-            Triangle res1 = new Triangle(1, 2, 5, 4, 5.5, 1.5);
-            Rectangle res2 = new Rectangle(5, 10);
-            Circle res3 = new Circle(3);
-
-            Console.WriteLine(res1.FirstSlide + " " + res1.SecondSlide + " "  + res1.ThirdSlide);
-            Console.WriteLine(res3.Diameter);
-
-            Console.WriteLine(res.GetHeight());
-            Console.WriteLine(res.GetWidth());
-            Console.WriteLine(res.GetArea());
-            Console.WriteLine(res.GetPerimeter());
-
-            Console.WriteLine();
-
-            Console.WriteLine(res1.GetHeight());
-            Console.WriteLine(res1.GetWidth());
-            Console.WriteLine(res1.GetArea());
-            Console.WriteLine(res1.GetPerimeter());
-
-            Console.WriteLine();
-
-            Console.WriteLine(res2.GetHeight());
-            Console.WriteLine(res2.GetWidth());
-            Console.WriteLine(res2.GetArea());
-            Console.WriteLine(res2.GetPerimeter());
-
-            Console.WriteLine();
-
-            Console.WriteLine(res3.GetHeight());
-            Console.WriteLine(res3.GetWidth());
-            Console.WriteLine(res3.GetArea());
-            Console.WriteLine(res3.GetPerimeter());
+            Square firstSquare = new Square(6.25);
+            Triangle firstTriangle = new Triangle(1, 2, 5, 4, 5.5, 1.5);
+            Rectangle firstRectangle = new Rectangle(5, 10);
+            Circle firstCircle = new Circle(3);
+            Square secondSquare = new Square(6.25);
+            Triangle secondTriangle = new Triangle(1, 2, 5, 4, 5.5, 1.5);
+            Rectangle secondRectangle = new Rectangle(5, 10);
+            Circle secondCircle = new Circle(3);
+            Square thirdSquare = new Square(6.25);
+            Triangle thirdTriangle = new Triangle(1, 2, 5, 4, 5.5, 1.5);
+            Rectangle thirdRectangle = new Rectangle(5, 10);
 
             Console.ReadKey();
         }

775ed38a2dd83a0bffa639bfc7ee9d7e5b58ea27
DenUsm <denchikiiii191@gmail.com>
2019-02-08 13:55:28 +0700

    Add Triangle class

--- CourseTask/Shape/Program.cs
+++ CourseTask/Shape/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shape
 {
@@ -10,6 +6,22 @@ namespace Shape
     {
         static void Main(string[] args)
         {
+            Square res = new Square(6.25);
+            Triangle res1 = new Triangle(1, 2, 5, 4, 5.5, 1.5);
+
+            Console.WriteLine(res.GetHeigth());
+            Console.WriteLine(res.GetWidth());
+            Console.WriteLine(res.GetArea());
+            Console.WriteLine(res.GetPerimeter());
+
+            Console.WriteLine();
+
+            Console.WriteLine(res1.GetHeigth());
+            Console.WriteLine(res1.GetWidth());
+            Console.WriteLine(res1.GetArea());
+            Console.WriteLine(res1.GetPerimeter());
+
+            Console.ReadKey();
         }
     }
 }

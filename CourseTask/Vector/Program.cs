5c36a97a5cf29e18444196531a7f98c85ad4152a
Denis <denchikiiiiUsm@yandex.ru>
2019-02-09 20:21:07 +0700

    Create Vector class and add some Consructor

--- CourseTask/Vector/Program.cs
+++ CourseTask/Vector/Program.cs
@@ -6,6 +6,23 @@ namespace Vector
     {
         static void Main(string[] args)
         {
+            Vector testEx = new Vector(-5);
+            Vector testEx1 = new Vector(4, new double[] { 1, 2, 3, 4, 5 });
+
+            Vector vec1 = new Vector(5);
+            Vector vec2 = new Vector(vec1);
+            Vector vec3 = new Vector(new double[] { 1.2, 5.5, 4.7, 8, 6.4 });
+            Vector vec4 = new Vector(5, new double[] { 1, 2, 3, 4, 5 });
+
+
+            Vector[] mass = new Vector[] { vec1, vec2, vec3, vec4 };
+
+            foreach (Vector val in mass)
+            {
+                Console.WriteLine(val);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CourseTask/Vector/Vector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Vector
+{
+    class Vector
+    {
+        public double[] Components { get; set; }
+
+        public Vector(int n)
+        {
+            try
+            {
+                if (n <= 0)
+                {
+                    throw new ArgumentException("Size of vector must be > 0", nameof(n));
+                }
+                Components = new double[n];
+                Components.SetValue(0, Components.Length - 1);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("{0} do not create {1}", GetType().Name, ex);
+            }
+        }
+
+        public Vector(Vector vector)
+        {
+            Components = vector.Components;
+        }
+
+        public Vector(double[] components)
+        {
+            Components = components;
+        }
+
+        public Vector(int n, double[] components)
+        {
+            try
+            {
+                if (components.Length > n)
+                {
+                    throw new ArgumentException("Components count must be <= size of vector", nameof(components));
+                }
+                Components = new double[n];
+                Components = components;
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("{0} do not create {1}", GetType().Name, ex);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "{" + string.Join(", ", Components) + "}";
+        }
+    }
+}

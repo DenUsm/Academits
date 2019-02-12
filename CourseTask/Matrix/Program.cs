ca96637e332d2ced4e43b838f3ca8b2e40bf9a87
DenUsm <denchikiiii191@gmail.com>
2019-02-12 11:22:16 +0700

    Create project Matrix

---
+++ CourseTask/Matrix/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(5, 5);
+            Console.WriteLine(matrix.ToString());
+
+            int n = matrix.N;
+            int m = matrix.M;
+
+            Console.ReadKey();
+        }
+    }
+}

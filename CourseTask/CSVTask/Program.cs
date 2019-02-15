4d01061668b168102c6378206858bc12b4ea4a7c
DenUsm <denchikiiii191@gmail.com>
2019-02-15 15:35:09 +0700

    Create project CSVTask

---
+++ CourseTask/CSVTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace CSVTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader readet = new StreamReader(@"Example.csv"))
+            {
+
+            }
+        }
+    }
+}

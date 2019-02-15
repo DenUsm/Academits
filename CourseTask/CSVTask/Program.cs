33f956d87073222c6381f7370270433ade4a71d5
DenUsm <denchikiiii191@gmail.com>
2019-02-15 19:14:29 +0700

    add read and split line string

--- CourseTask/CSVTask/Program.cs
+++ CourseTask/CSVTask/Program.cs
@@ -8,28 +8,49 @@ namespace CSVTask
     {
         static void Main(string[] args)
         {
-            string[] str = {";"};
+            string strAllFile;
             using (StreamReader reader = new StreamReader(@"Example.csv", Encoding.Default))
             {
                 string currentLine;
                 while((currentLine = reader.ReadLine()) != null)
                 {
-                    str = currentLine.Split(str, StringSplitOptions.RemoveEmptyEntries);
+                    string[] str1 = currentLine.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    //CrateHTMLFile(str[0]);
                 }
+                
+
+            //пока запятая не в конце чиатть по одной строке и накапливать
             }
+           
 
 
-            foreach(string value in str)
-            {
-                Console.WriteLine(value);
-            }
+            //foreach(string value in str)
+            //{
+            //    Console.WriteLine(value);
+            //}
 
             Console.ReadKey();
         }
 
         //public static void CrateHTMLFile(string str)
         //{
+        //    int indexInitial = 0;
+        //    int entriesCount = 0;
         //
+        //    while (true)
+        //    {
+        //        int findIndex = findString.IndexOf(compareString, indexInitial);
+        //        if (findIndex == -1)
+        //        {
+        //            break;
+        //        }
+        //        indexInitial = findIndex + compareString.Length;
+        //        entriesCount++;
+        //    }
+        //    return entriesCount;
+        //
+        //    int index = str.IndexOf(',');
+        //    string row1 = str.Substring(0, index);
         //}
     }
 }

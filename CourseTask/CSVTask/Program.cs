34290adc0347c2139d52819a7c91cf92b7ad0fd6
DenUsm <denchikiiii191@gmail.com>
2019-02-25 18:01:16 +0700

    in progress

--- CourseTask/CSVTask/Program.cs
+++ CourseTask/CSVTask/Program.cs
@@ -8,48 +8,68 @@ namespace CSVTask
     {
         static void Main(string[] args)
         {
-            using (StreamReader reader = new StreamReader(@"Test.txt", Encoding.Default))
+            using (StreamReader reader = new StreamReader(@"Example.csv", Encoding.Default))
             {
-                string currentLine;
-                while((currentLine = reader.ReadLine()) != null)
+                string firstAttributeEndCellWithQuotes = ",\"";
+                string secondAttributeEndCellWithQuotes = "\",";
+                string attributeEndCellWithoutQuotes = ",";
+                string attributeCommaOrBreakLine = "\"";
+
+                string str;
+
+                while ((str = reader.ReadLine()) != null)
                 {
-                    string[] str1 = currentLine.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-                    //CrateHTMLFile(str[0]);
+                    int indexInitial = 0;
+
+                    while (true)
+                    {
+                        //Проверка начинается ли ячейка с кавычек
+                        if (!str[indexInitial].ToString().Equals(attributeCommaOrBreakLine))
+                        {                     
+                            int indexEndCell = str.IndexOf(attributeEndCellWithoutQuotes, indexInitial);
+
+                            if (indexEndCell == -1)
+                            {
+                                break;
+                            }
+
+                            int lengthAttribute = attributeEndCellWithoutQuotes.Length;
+                            string result = "<td>" + str.Substring(indexInitial, indexEndCell) + "</td>";
+                            indexInitial = indexEndCell + lengthAttribute;
+                        }
+                        else
+                        {
+                            //Находим индексы возможных вариантов окончания ячейки
+                            int firstIndexEndCell = str.IndexOf(firstAttributeEndCellWithQuotes, indexInitial);
+                            int secondIndexEndCell = str.IndexOf(secondAttributeEndCellWithQuotes, indexInitial);
+
+                            //вариант окончания ячейки ,\" и следом ,
+                            if ((firstIndexEndCell != -1) && (str[firstIndexEndCell + firstAttributeEndCellWithQuotes.Length].ToString() == ","))
+                            {
+                                int lengthAttribute = firstAttributeEndCellWithQuotes.Length;
+                                string result = str.Substring(firstIndexEndCell, str.Length - firstIndexEndCell - lengthAttribute);
+                                indexInitial = firstIndexEndCell + lengthAttribute;
+                            }
+                            //вариант окончания ячейки \", и следом нет ,
+                            else if ((secondIndexEndCell != -1) && (str[secondIndexEndCell + secondAttributeEndCellWithQuotes.Length].ToString() != ","))
+                            {
+                                int lengthAttribute = secondAttributeEndCellWithQuotes.Length;
+                                string result = str.Substring(secondIndexEndCell, str.Length - secondIndexEndCell - lengthAttribute);
+                                indexInitial = secondIndexEndCell + lengthAttribute;
+                            }
+                            //нет окончания значит перенос строки
+                            else
+                            {
+                                int indexEndCell = str.IndexOf(attributeCommaOrBreakLine, indexInitial);
+                                int lengthAttribute = attributeCommaOrBreakLine.Length;
+                                string result = "<td>" + str.Substring(indexInitial + lengthAttribute, str.Length - indexInitial - lengthAttribute) + "<br/>";
+                                indexInitial += lengthAttribute;
+                            }
+                        }
+                    }
                 }
-                
-
-            //пока запятая не в конце чиатть по одной строке и накапливать
             }
-           
-
-
-            //foreach(string value in str)
-            //{
-            //    Console.WriteLine(value);
-            //}
-
             Console.ReadKey();
         }
-
-        //public static void CrateHTMLFile(string str)
-        //{
-        //    int indexInitial = 0;
-        //    int entriesCount = 0;
-        //
-        //    while (true)
-        //    {
-        //        int findIndex = findString.IndexOf(compareString, indexInitial);
-        //        if (findIndex == -1)
-        //        {
-        //            break;
-        //        }
-        //        indexInitial = findIndex + compareString.Length;
-        //        entriesCount++;
-        //    }
-        //    return entriesCount;
-        //
-        //    int index = str.IndexOf(',');
-        //    string row1 = str.Substring(0, index);
-        //}
     }
 }

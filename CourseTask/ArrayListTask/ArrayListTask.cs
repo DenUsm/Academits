8d4a0cb9a0bf9ae07bd0aaf975bb0d1021730490
DenUsm <denchikiiii191@gmail.com>
2019-03-12 18:28:37 +0700

    ArrayListTask in progress

--- CourseTask/ArrayListTask/ArrayListTask.cs
+++ CourseTask/ArrayListTask/ArrayListTask.cs
@@ -6,6 +6,22 @@ namespace ArrayListTask
     {
         static void Main(string[] args)
         {
+            MyList<int> my = new MyList<int>();
+
+            Console.WriteLine("---------------------------------------Test Add---------------------------------");
+            my.Add(10);
+            my.Add(20);
+            my.Add(30);
+            my.Add(40);
+            my.Add(50);
+            my.Add(60);
+            Console.WriteLine("Values {0} - {1}", my.ToString(), my.Count);
+
+            Console.WriteLine("---------------------------------------Test RemoveAt---------------------------------");
+            my.RemoveAt(3);
+            Console.WriteLine("Values {0} - {1}", my.ToString(), my.Count);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CourseTask/ArrayListTask/MyList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayListTask
+{
+    class MyList<T> : IList<T>
+    {
+        private T[] Items = new T[10];
+        private int Length;
+
+        public MyList()
+        {
+
+        }
+
+        public MyList(int capacity)
+        {
+            Items = new T[capacity];
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] old = Items;
+            Items = new T[old.Length * 2];
+            Array.Copy(old, 0, Items, 0, old.Length);
+        }
+
+        private void Check(int index)
+        {
+            if ((index > Items.Length) || (index < 0))
+            {
+                throw new ArgumentException("index must be less of items array length", nameof(index));
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                Check(index);
+                return Items[index];
+            }
+            set
+            {
+                Check(index);
+                Items[index] = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (Length >= Items.Length)
+            {
+                IncreaseCapacity();
+            }
+            Items[Length] = value;
+            Length++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Check(index);
+            if (index < Length)
+            {
+                Array.Copy(Items, index + 1, Items, index, Length - index);
+            }
+            Length--;
+        }
+
+        public int Count => Length;
+
+
+
+
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+     
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("[");
+            str.Append(String.Join(", ", Items));
+            str.Append("]");
+
+            return str.ToString();
+        }
+
+    }
+}

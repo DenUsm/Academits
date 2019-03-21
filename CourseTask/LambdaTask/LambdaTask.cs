e5ac684184842837d777074b7d7cb66195801da8
DenUsm <denchikiiii191@gmail.com>
2019-03-21 13:40:38 +0700

    Create project LambdaTask

---
+++ CourseTask/LambdaTask/LambdaTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaTask
+{
+    class LambdaTask
+    {
+        static void Main(string[] args)
+        {
+            var items = new[] { "d2", "a2", "b1", "b3", "c" }
+            .Select(s =>
+            {
+                Console.WriteLine("map: " + s);
+                return s.ToUpper();
+            })
+            .Where(s =>
+            {
+                Console.WriteLine("filter: " + s);
+            return s.StartsWith("A");
+            });
+            foreach (var s in items) { Console.WriteLine("forEach: " + s); }
+
+            Console.ReadKey();
+        }
+    }
+}

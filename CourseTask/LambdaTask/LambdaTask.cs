6568ddf1800c922925953711be6a4fdf98766a13
DenUsm <denchikiiii191@gmail.com>
2019-03-25 15:11:55 +0700

    Исправлены замечания:
    Lambda:
    1. peaple - опечатка
    2. Можно не писать круглые скобки если используется инициализатор коллекции: http://joxi.ru/J2b5EplI04M79m
    3. Обычно это так форматируют: http://joxi.ru/D2PBROQcqdWPDr
    4. А цепочки так: http://joxi.ru/krD9eVDfK0dYRm
    Это надо везде поправить
    5. Сейчас тут коллекция переводится в список дважды, это неэффективно: http://joxi.ru/xAeel90hRYVEJA
    6. Средний возраст нужно посчитать через стандартную функцию
    7. В последней задаче:
    - надо вывести результат
    - можно обойтись без компаратора
    - функции в Select должны быть чистыми - ничего не печатать, ничего не менять снаружи себя

--- CourseTask/LambdaTask/LambdaTask.cs
+++ CourseTask/LambdaTask/LambdaTask.cs
@@ -8,21 +8,26 @@ namespace LambdaTask
     {
         static void Main(string[] args)
         {
-            List<Person> peaple = new List<Person>() {
-            new Person("Ivan", 25),
-            new Person("Denis", 40),
-            new Person("Oleg", 20),
-            new Person("Ivan", 56),
-            new Person("Aleksey", 9),
-            new Person("Roman", 10),
-            new Person("Oleg", 17),
-            new Person("Mikhail", 32),
-            new Person("Petr", 45),
-            new Person("Genadiy", 4),
-            new Person("Egor", 78) };
+            List<Person> people = new List<Person>
+            {
+                new Person("Ivan", 25),
+                new Person("Denis", 40),
+                new Person("Oleg", 20),
+                new Person("Ivan", 56),
+                new Person("Aleksey", 9),
+                new Person("Roman", 10),
+                new Person("Oleg", 17),
+                new Person("Mikhail", 32),
+                new Person("Petr", 45),
+                new Person("Genadiy", 4),
+                new Person("Egor", 78)
+            };
 
             //Получить список уникальных имен
-            List<string> uniqueList = peaple.Select(p => p.Name).ToList().Distinct().ToList();
+            List<string> uniqueList = people
+                .Select(p => p.Name)
+                .Distinct()
+                .ToList();
 
             foreach (string val in uniqueList)
             {
@@ -35,19 +40,23 @@ namespace LambdaTask
 
             //получить список людей младше 18, посчитать для них средний возраст
             Console.WriteLine();
-            List<Person> personUnder = peaple.Where(p => p.Age < 18).ToList();
+            List<Person> personUnder = people
+                .Where(p => p.Age < 18)
+                .ToList();
 
             foreach (Person person in personUnder)
             {
                 Console.WriteLine(person.ToString());
             }
 
-            int averageOld = personUnder.Sum(p => p.Age) / personUnder.Count;
+            double averageOld = personUnder.Average(p => p.Age);
             Console.WriteLine("Average old peaple under 18 is {0}", averageOld);
 
             //при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст
             Console.WriteLine();
-            Dictionary<string, int> map = peaple.GroupBy(p => p.Name).ToDictionary(p => p.Key, p => p.Sum(x => x.Age) / p.ToList().Count);
+            Dictionary<string, double> map = people
+                .GroupBy(p => p.Name)
+                .ToDictionary(p => p.Key, p => p.Average(a => a.Age));
 
             foreach (var pair in map)
             {
@@ -56,32 +65,17 @@ namespace LambdaTask
 
             //получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
             Console.WriteLine();
-            var result = peaple.Where(p => ((p.Age >= 20) && (p.Age <= 45)))
-                               .OrderBy(p => p.Age, new MyComparer())
-                               .Select(p =>
-                               {
-                                   Console.WriteLine("Name: {0}", p.Name);
-                                   return p;
-                               }).ToList();
+            var result = people
+                .Where(p => ((p.Age >= 20) && (p.Age <= 45)))
+                .OrderByDescending(p => p.Age)
+                .ToList();
 
+            foreach (var value in result)
+            {
+                Console.WriteLine(value);
+            }
 
             Console.ReadKey();
-        }
-
-        class MyComparer : IComparer<int>
-        {
-            public int Compare(int x, int y)
-            {
-                if (x > y)
-                {
-                    return -1;
-                }
-                else if (x < y)
-                {
-                    return 1;
-                }
-                return 0;
-            }
         }
     }
 }

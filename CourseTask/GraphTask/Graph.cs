1d9e7f74c6279cbf3688ee2e4b41ff793c5acbc2
DenUsm <denchikiiii191@gmail.com>
2019-03-27 18:11:35 +0700

    Исправлены замечания: 1. Пусть main будет в отдельном классе
    2. Поля должны быть private и не static.
    Пусть матрица заполняется в графе через конструктор
    
    3. Должен быть массив [,]
    
    4. Пусть обходы принимают Action<int>.
    Его надо вызвать для каждой вершины вместо печати.
    А в Main как раз туда можно передать функцию печати
    
    5. Пусть печатаются ровно те номера вершин, которые есть, т.е. с 0
    
    6. bool не нужно проверять на true/false через == и !=
    
    7. Лишний continue: http://joxi.ru/xAeel90hRYE3NA
    
    8. InitialVisited - имя начинается не с глагола

---
+++ CourseTask/GraphTask/Graph.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphTask
+{
+    class Graph
+    {
+        private int[,] graph;
+
+        private bool[] visited;
+
+        public Graph(int[,] graph)
+        {
+            this.graph = graph;
+            visited = new bool[graph.GetLength(0)];
+        }
+
+        private void StatusVisitedInitial()
+        {
+            for (int i = 0; i < visited.Length; i++)
+            {
+                visited[i] = false;
+            }
+        }
+
+        public void WayGoWide(int initialNode, Action<int> work)
+        {
+            //Инициализируем массив пустыми значениями
+            StatusVisitedInitial();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(initialNode);
+
+            while (queue.Count != 0)
+            {
+                int nextNode = queue.Dequeue();
+
+                int length = graph.GetLength(0);
+                int[] node = new int[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    node[i] = graph[nextNode, i];
+                }
+
+                //проверяем посещали ли данный узел
+                if (!visited[nextNode])
+                {
+                    visited[nextNode] = true;
+                    work(nextNode);
+
+                    for (int i = 0; i < node.Length; i++)
+                    {
+                        if (node[i] == 1 && !visited[i])
+                        {
+                            queue.Enqueue(i);
+                        }
+                    }
+                }
+
+                //проверяем остались ли какие либо не посещенные узлы
+                if (queue.Count == 0)
+                {
+                    for (int i = 0; i < visited.Length; i++)
+                    {
+                        if (!visited[i])
+                        {
+                            queue.Enqueue(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void WayGoDepth(int initialNode, Action<int> work)
+        {
+            //Инициализируем массив пустыми значениями
+            StatusVisitedInitial();
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(initialNode);
+
+            while (stack.Count != 0)
+            {
+                int nextNode = stack.Pop();
+
+                int length = graph.GetLength(0);
+                int[] node = new int[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    node[i] = graph[nextNode, i];
+                }
+
+                //проверяем посещали ли данный узел
+                if (!visited[nextNode])
+                {
+                    visited[nextNode] = true;
+                    work(nextNode);
+
+                    for (int i = 0; i < node.Length; i++)
+                    {
+                        if (node[i] == 1 && !visited[i])
+                        {
+                            stack.Push(i);
+                        }
+                    }
+                }
+
+                //проверяем остались ли какие либо не посещенные узлы
+                if (stack.Count == 0)
+                {
+                    for (int i = 0; i < visited.Length; i++)
+                    {
+                        if (!visited[i])
+                        {
+                            stack.Push(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
